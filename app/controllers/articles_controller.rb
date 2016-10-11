6a84b01e37a7704f0df5c2b6a6c9344fb69a9b79
jueluddin <juddin@panopen.com>
2016-10-11 12:59:46 -0400

    fix article update def

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,9 +3,25 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+
+  end
+
   def create
     #render plain: params[:article].inspect
-     @article = Article.new(article_params)
+    @article = Article.new(article_params)
     # @article.save
     # redirect_to article_path(@article)
 
